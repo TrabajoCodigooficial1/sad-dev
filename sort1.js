1a9b24781b6f72c93f16fc3c9cfc0c7ec5be35c1
Andrés Blanco Ferro <heraclitesnoesparmenides@gmail.com>
2025-01-14 13:06:02 +0100

    Create sort1.js

---
+++ sort1.js
@@ -0,0 +1,9 @@
+// sort.js
+
+// Ejemplo: Ordenar un array de números de menor a mayor
+const numeros = [5, 3, 8, 1, 2];
+
+// Usamos la función sort() para ordenar el array
+numeros.sort((a, b) => a - b);
+
+console.log("Números ordenados:", numeros);
