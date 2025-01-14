14921b3b20d9c79bcbce53d3788b5dae61d0ff40
Andrés Blanco Ferro <heraclitesnoesparmenides@gmail.com>
2025-01-14 13:09:42 +0100

    Revert "added simp chares"

--- sort1.js
+++
@@ -1,9 +0,0 @@
-// sort.js
-
-// Ejemplo: Ordenar un array de números de menor a mayor
-const numeros = [5, 3, 8, 1, 2];
-
-// Usamos la función sort() para ordenar el array
-numeros.sort((a, b) => a - b);
-
-console.log("Números ordenados:", numeros);
